2cb777384e11348cb7b3e1b594b8a679d19d4eb9
shamssaniat <shams.saniat@northsouth.edu>
2025-01-14 18:28:09 +0600

    added functionality in JSX

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,34 @@
-
+import { useState } from 'react'
 import './App.css'
 
 function App() {
 
+  let [data, setData] = useState("");
+  let [item, setItem] = useState([]);
 
+  const handleChange = (e) => {
+    setData(e.target.value);
+  }
+
+  const handleSave = () => {
+    setItem([...item, data]); // Corrected "items" to "item"
+  }
 
   return (
     <div className='App'>
-      
+      <input type="text"
+        value={data}
+        onChange={handleChange}
+      />
+      <br />
+      <button onClick={handleSave}>save</button>
+      <ul>
+        {
+          item.map((item, index) => {
+            return <li key={index}>{item}</li>; // Removed extra ">"
+          })
+        }
+      </ul>
     </div>
   )
 }
